60fd7d460ae038079efb54d2377dfcded7eaad29
Riki-Coddee <rikeshshrestha9821@gmail.com>
2025-03-12 10:45:18 +0545

    Making header and footer responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [sidebarStatus, setSidebarStatus]= useState(false);
+
+  const showSidebar =()=>{
+    if(sidebarStatus===true) setSidebarStatus(false);
+    else setSidebarStatus(true);
+  }
   return (
     <div class="hmsNavbar">
       <div class="navbar">
@@ -9,21 +15,26 @@ const Navbar = () => {
         </div>
         <div class="navbar-itemlist">
           <ul className="navbar-navv">
-            <li class="navbar-item">Home</li>
-            <li class="navbar-item">About-Us</li>
-            <li class="navbar-item">Services</li>
-            <li class="navbar-item">Contact</li>
+            <li class="navbar-item"><a href="/">Home</a></li>
+            <li class="navbar-item"><a href="/">About-Us</a></li>
+            <li class="navbar-item"><a href="/">Services</a></li>
+            <li class="navbar-item"><a href="/">Contact</a></li>
           </ul>
         </div>
         <div class="navbar-btn">
           <div class="loginBtn">
-            <button>Login</button>
+            <button>Get Started</button>
           </div>
-          <div class="signupBtn">
-            <button>Signup</button>
-          </div>
+          <i class={(sidebarStatus===true)?"fa-solid fa-xmark":"fa-solid fa-list"} onClick={showSidebar}></i>
         </div>
       </div>
+      <div class={(sidebarStatus===true)?"dropdown_menu open":"dropdown_menu"}>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">About-Us</a></li>
+            <li><a href="/">Services</a></li>
+            <li><a href="/">Contact</a></li>
+            <li><button>Get Started</button></li>
+        </div>
     </div>
   );
 };
